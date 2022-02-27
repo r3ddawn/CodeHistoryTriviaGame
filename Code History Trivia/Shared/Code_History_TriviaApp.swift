ec93b4102f1393ce6c7727056c76b34abf9fe30d
Eric <ejohnson1028@gmail.com>
2022-02-27 08:04:35 -0800

    Initial Commit
    Initial Commit

---
+++ Code History Trivia/Shared/Code_History_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_History_TriviaApp.swift
+//  Shared
+//
+//  Created by Eric Johnson on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_History_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
